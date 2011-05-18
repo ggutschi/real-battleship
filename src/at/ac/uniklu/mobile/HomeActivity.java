046066cdba89790835302f438a2c9ca21252fa52
developer@xgx.at <developer@xgx.at@de5a7e7b-5c56-dc37-75bc-424bdb6bafd4>
2011-05-18 09:16:37 +0000

    list return intent to home

--- src/at/ac/uniklu/mobile/HomeActivity.java
+++ src/at/ac/uniklu/mobile/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import at.ac.uniklu.mobile.db.Challenge;
 import at.ac.uniklu.mobile.util.Constants;
 
 import com.google.android.maps.GeoPoint;
@@ -21,14 +22,17 @@ public class HomeActivity extends MapActivity {
         setContentView(R.layout.home);
         
         Log.d(Constants.LOG_TAG, "initialize mapview");
-        configureMapView();
+        configureMapView(null);
         
         // set up button listener for menu selection
         final Button button = (Button) findViewById(R.id.button_change_challenge);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	Log.d(Constants.LOG_TAG, "change challenge button clicked");
-                startActivity(new Intent(HomeActivity.this, ChallengeListActivity.class));
+            	startActivityForResult(new Intent(HomeActivity.this, ChallengeListActivity.class), 
+            			Constants.CMD_CODE_CHANGE_CHALLENGE);
+            	
+                //startActivity(new Intent(HomeActivity.this, ChallengeListActivity.class));
             }
         });
         
@@ -44,15 +48,37 @@ public class HomeActivity extends MapActivity {
         
     }
     
+    
+    /* (non-Javadoc)
+     * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        Log.d(Constants.LOG_TAG, "on activity result, request code: " + requestCode + " , resultCode: " + resultCode);
+        
+        if (requestCode == Constants.CMD_CODE_CHANGE_CHALLENGE && resultCode == RESULT_OK) {
+            // new challenge selected
+        	ChallengeListModel listModel = ChallengeListModel.getInstance(this);
+        	Challenge challenge = listModel.getChallengeById(data.getExtras().getInt(Challenge.FIELD_ID));
+        	configureMapView(challenge);
+        }
+    }
+    
     /**
      * configures the google map view by setting default values
      */
-    private void configureMapView() {
+    private void configureMapView(Challenge c) {
+    	GeoPoint geoPoint = null;
     	MapView mapView = (MapView) findViewById(R.id.challenge_map);
         mapView.setBuiltInZoomControls(true);
         MapController mapController = mapView.getController();
         mapController.setZoom(10);
-        GeoPoint geoPoint = new GeoPoint ((int)(Constants.DEFAULT_LATITUDE * 1E6), ((int)(Constants.DEFAULT_LONGITUDE * 1E6)));
+        if (c != null)
+        	geoPoint = c.getLocationLeftTop();
+        else
+        	geoPoint = new GeoPoint ((int)(Constants.DEFAULT_LATITUDE * 1E6), ((int)(Constants.DEFAULT_LONGITUDE * 1E6)));
         mapController.animateTo(geoPoint);
     }
     
