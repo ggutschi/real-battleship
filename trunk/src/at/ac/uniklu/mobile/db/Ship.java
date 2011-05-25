2765d23e9c6fd3bbe7c7eff84f0f78568b313b23
developer@xgx.at <developer@xgx.at@de5a7e7b-5c56-dc37-75bc-424bdb6bafd4>
2011-05-25 07:49:10 +0000

    new ship object which contains ship positions

---
+++ trunk/src/at/ac/uniklu/mobile/db/Ship.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package at.ac.uniklu.mobile.db;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Ship {
+	
+	/** id of the ship **/
+	private int id;
+	/** destroyed-state of a ship **/
+	private boolean destroyed;
+	
+	/** list of ship positions **/
+	private ArrayList<ShipPosition> shipPositions;
+	
+	/** json field label for id **/
+	private static final String FIELD_ID = "id";
+	/** json field label for destroyed-flag **/
+	private static final String FIELD_DESTROYED = "destroyed";
+	
+	private static final String FIELD_SHIPPOSITIONS = "shippositions";
+	
+	public Ship(JSONObject jsonObject) throws JSONException {		
+		id = jsonObject.getInt(FIELD_ID);
+		destroyed = jsonObject.getBoolean(FIELD_DESTROYED);
+		
+		JSONArray jsonShipPositions = jsonObject.getJSONArray(FIELD_SHIPPOSITIONS);
+		shipPositions = new ArrayList<ShipPosition>();
+		  for (int i = 0; i < jsonShipPositions.length(); i++) 
+		  {
+			  ShipPosition sp = new ShipPosition(jsonShipPositions.getJSONObject(i));
+			  shipPositions.add(sp);
+		  }		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+
+	public void setDestroyed(boolean destroyed) {
+		this.destroyed = destroyed;
+	}
+
+	public ArrayList<ShipPosition> getShipPositions() {
+		return shipPositions;
+	}
+
+	public void setShipPositions(ArrayList<ShipPosition> shipPositions) {
+		this.shipPositions = shipPositions;
+	}
+
+}
